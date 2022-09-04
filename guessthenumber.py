d6d437439a56ab17954cbdf40b1ff0d907743051
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-09-04 23:39:49 +0400

    add conditionals

--- guessthenumber.py
+++ guessthenumber.py
@@ -8,4 +8,10 @@ def guess_the_number_user(lower_bound,upper_bound):
     user_guess = math.inf
 
     while number_to_guess != user_guess:
-        user_guess = int(input(f"Guess the number between {lower_bound} and {upper_bound}"))
+        user_guess = int(input(f"Guess the number between {lower_bound} and {upper_bound}: "))
+        if user_guess < number_to_guess:
+            print("Too small, Try again: ")
+        elif user_guess > number_to_guess:
+            print("Too big, Try again: ")
+
+guess_the_number_user(lower_bound,upper_bound)
