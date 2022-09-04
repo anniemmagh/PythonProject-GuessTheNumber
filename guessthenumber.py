1ec7fc367435dfeb90f8e530f2243d2c09f1d7a6
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-09-04 23:30:05 +0400

    add while loop

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,7 +1,11 @@
 from random import randrange
-
+import math
 lower_bound = int(input("Please enter lower bound: ")) 
 higher_bound = int(input("Please enter higher bound: "))
 
 def guess_the_number_user(lower,higher):
     number_to_guess = randrange(lower,higher)
+    user_guess = math.inf
+
+    while number_to_guess != user_guess:
+        user_guess = int(input("Guess the number between"))
