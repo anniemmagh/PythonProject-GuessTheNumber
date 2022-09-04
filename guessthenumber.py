a34a0bbaee83848703c71f6481b2690630003593
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-09-04 23:23:21 +0400

    import randrange

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,5 +1,7 @@
+from random import randrange
+
 lower_bound = int(input("Please enter lower bound: ")) 
 higher_bound = int(input("Please enter higher bound: "))
 
-def guess_the_number_user():
-    number_to_guess = 
+def guess_the_number_user(lower,higher):
+    number_to_guess = randrange(lower,higher)
