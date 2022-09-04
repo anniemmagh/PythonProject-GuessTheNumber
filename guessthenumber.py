79f204be7ba196761095ab7dc3dccb73d1b9c489
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-09-04 23:47:16 +0400

    add guess counter

--- guessthenumber.py
+++ guessthenumber.py
@@ -6,12 +6,16 @@ upper_bound = int(input("Please enter upper bound: "))
 def guess_the_number_user(lower_bound,upper_bound):
     number_to_guess = randrange(lower_bound,upper_bound)
     user_guess = math.inf
+    guess_counter = 0
 
     while number_to_guess != user_guess:
+        guess_counter = guess_counter + 1
         user_guess = int(input(f"Guess the number between {lower_bound} and {upper_bound}: "))
         if user_guess < number_to_guess:
             print("Too small, Try again: ")
         elif user_guess > number_to_guess:
             print("Too big, Try again: ")
+
+    print( f"Congratulation! You guessed the number {number_to_guess} in {guess_counter} tries")
 
 guess_the_number_user(lower_bound,upper_bound)
